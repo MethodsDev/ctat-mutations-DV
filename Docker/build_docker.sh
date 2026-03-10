f69ae3d2838fdbd3fad951189d12824f564e347e
Brian Haas <bhaas@broadinstitute.org>
2026-03-10 01:08:02 +0000

    docker update

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -5,7 +5,7 @@ set -e
 VERSION=`cat VERSION.txt`
 
 # Build from parent directory context to include .git and all source files
-docker build -f Dockerfile -t trinityctat/ctat_mutations:$VERSION ..
-docker build -f Dockerfile -t trinityctat/ctat_mutations:latest ..
+docker build -f Dockerfile -t trinityctat/ctat_mutations_dv:$VERSION .
+docker build -f Dockerfile -t trinityctat/ctat_mutations_dv:latest .
 
 
